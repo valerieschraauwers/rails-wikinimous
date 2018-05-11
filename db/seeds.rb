12042abdc3575e47a9c0f621adb669317ce36e0a
valerie schraauwers <valerieschraauwers@gmail.com>
2018-05-11 17:23:16 +0800

    Add articles manager

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+10.times do
+  article =  Article.new(
+    title: Faker::Book.title,
+    content: Faker::Hipster.paragraphs.join,
+  )
+  article.save
+end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
